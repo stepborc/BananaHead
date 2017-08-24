cc7ab93948c41372d3836e602e9594ac4486f082
root <root@bpi.Speedport_W_724V_09011603_05_012>
2017-08-24 10:21:51 +0200

    install tvheadend

---
+++ installTvheadend.sh
@@ -0,0 +1,6 @@
+sudo apt-get install apt-transport-https
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+echo "deb https://dl.bintray.com/tvheadend/deb jessie release" | sudo tee -a /etc/apt/sources.list.d/tvheadend.list 
+sudo apt-get update
+sudo apt-get install tvheadend
+
