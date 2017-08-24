1566983eb7033ec5503a0c29bd6f038aab7337bb
stepborc <sbgithub@gmail.com>
2017-08-24 15:18:29 +0200

    replace one more sudo

--- installTvheadend.sh
+++ installTvheadend.sh
@@ -2,7 +2,8 @@
 #apt-get install apt-transport-https
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
-echo "deb https://dl.bintray.com/tvheadend/deb jessie release" | sudo tee -a /etc/apt/sources.list.d/tvheadend.list 
+#echo "deb https://dl.bintray.com/tvheadend/deb jessie release" | sudo tee -a /etc/apt/sources.list.d/tvheadend.list 
+echo "deb https://dl.bintray.com/tvheadend/deb jessie release" | tee -a /etc/apt/sources.list.d/tvheadend.list 
 #sudo apt-get update
 apt-get update
 #sudo apt-get install tvheadend
